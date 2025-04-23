c9213a5e4ce68929e5bf0f63f6305ebc36adde37
piotr-bledowski-student <63125128+piotr-bledowski@users.noreply.github.com>
2025-04-23 19:30:22 +0200

    Implement data processing utils

---
+++ data_processing/create_landmark_dataset.py
@@ -0,0 +1,7 @@
+import numpy as np
+import mediapipe as mp
+from utils import extract_landmarks
+
+
+def create_dataset():
+    pass
---
+++ data_processing/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+import mediapipe as mp
+import cv2
+
+
+def extract_landmarks(img: np.ndarray) -> np.ndarray:
+    holistic = mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence=0.75)
+    results = holistic.process(img)
+    r = results.right_hand_landmarks
+    l = results.left_hand_landmarks
+
+    landmarks = None
+
+    if r:
+        landmarks = [[landmark.x, landmark.y, landmark.z] for landmark in r.landmark]
+    elif l:
+        landmarks = [[landmark.x, landmark.y, landmark.z] for landmark in l.landmark]
+    
+    return np.array(landmarks)
+
+
+def base_distance_transform(landmarks: np.ndarray) -> np.ndarray:
+    result = []
+    base = landmarks[0]
+
+    for i in range(1, len(landmarks)):
+        # calculate euclidean distance between each landmark and base
+        result.append(np.linalg.norm(base, landmarks[i]))
+    
+    return result
+
+
+img = cv2.imread('data/136.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print(extract_landmarks(img).shape)
